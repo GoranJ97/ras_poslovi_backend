a48eaadfe8c7860255d53f2d7660942f29876c21
GoranJ97 <goran.jovanovic997@gmail.com>
2022-06-16 15:06:10 +0200

    Update composer.json, routes.yaml, and 9 more files...

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,7 +2,100 @@
 
 namespace App\Controller;
 
-class PostController
+use App\Entity\Post;
+use App\Services\PostService;
+use App\Services\ResponseService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PostController extends AbstractController
 {
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    )
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        $postService = new PostService($this->entityManager);
+        $postList = $postService->index($request);
+
+        return ResponseService::sendList(
+            $postList,
+            $this->entityManager->getRepository(Post::class)->count([])
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
+     */
+    public function create(
+        Request                $request,
+        EntityManagerInterface $entityManager
+    ): JsonResponse
+    {
+        try {
+            $postService = new PostService($this->entityManager);
+            $postService->create($request, $entityManager, $this->getUser());
+
+            return ResponseService::sendItem([], 'post_created', Response::HTTP_CREATED);
+        } catch (\Exception $exception) {
+
+            return ResponseService::sendItem(
+                [],
+                $exception->getMessage(),
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+    }
+
+    /**
+     * @param $id
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    public function show($id, Request $request): JsonResponse
+    {
+        $postService = new PostService($this->entityManager);
+        $post = $postService->show($id);
+
+        return ResponseService::sendItem($post);
+    }
+
+    /**
+     * @param $id
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    public function delete(
+        $id,
+        EntityManagerInterface $entityManager
+    ):JsonResponse
+    {
+        $postService = new PostService($this->entityManager);
+        $postService->delete($id , $entityManager);
+
+        return ResponseService::sendItem([], 'post_deleted', Response::HTTP_OK);
+
+    }
 
 }
--- src/Services/PostService.php
+++ src/Services/PostService.php
@@ -2,7 +2,113 @@
 
 namespace App\Services;
 
+use App\Entity\Post;
+use App\Entity\User;
+use App\Services\Validations\PostValidationService;
+use App\Traits\PostTrait;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class PostService
 {
+    use PostTrait;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    )
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function index(Request $request): array
+    {
+
+        $postsList = $this->entityManager
+            ->getRepository(Post::class)->findAll();
+        $posts = [];
+
+        foreach ($postsList as $post) {
+            array_push($posts, $this->getPostData($post));
+        }
+        return $posts;
+
+    }
+
+    /**
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param User $user
+     * @return Post
+     * @throws \Exception
+     */
+    public function create(
+        Request                $request,
+        EntityManagerInterface $entityManager,
+        User                   $user
+    ): Post
+    {
+        $postValidationService = new PostValidationService($this->entityManager);
+        $errors = $postValidationService->validatePost($request);
+
+        if (count($errors) > 0) {
+            throw new \Exception(json_encode($errors));
+        }
+
+        $post = new Post();
+        $post->setUser($user);
+        $post->setTitle($request->request->get('title'));
+        $post->setContent($request->request->get('content'));
+
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return $post;
+
+    }
+
+    /**
+     * @param $id
+     * @return array
+     * @throws \Exception
+     */
+    public function show($id): array
+    {
+        $post = $this->entityManager
+            ->getRepository(Post::class)->find(['id' => $id]);
+
+        if (!$post) {
+            throw new \Exception("Post with this id does not exist");
+        }
+        return $this->getPostData($post);
+    }
+
+    /**
+     * @param $id
+     * @param EntityManagerInterface $entityManager
+     * @return void
+     * @throws \Exception
+     */
+    public function delete($id, EntityManagerInterface $entityManager)
+    {
+        $post = $this->entityManager
+            ->getRepository(Post::class)->find(['id' => $id]);
+
+        if (!$post) {
+            throw new \Exception("Post does not exist");
+        }
+        $entityManager->remove($post);
+        $entityManager->flush();
+
+    }
 
 }
